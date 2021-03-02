9c93133118efe55b7c1d0abcf95ac6b72b008c24
Carlos G. Gavidia <cgavidiac@gmail.com>
2021-03-02 22:41:35 +0000

    Solucionario: Primer Laboratorio

--- primerlabo.py
+++ primerlabo.py
@@ -1,7 +1,16 @@
+import math
+
 def primer_laboratorio():
     # Ingrese la solucion en las lineas subsiguientes.
     # INICIO
+    primer_numero = int(input("Ingrese el valor de x: "))
+    segundo_numero = int(input("Ingrese el valor de y: "))
 
+    potencia = primer_numero ** segundo_numero
+    print("x ** y = " + str(potencia))
+
+    logaritmo = math.log(primer_numero, 2)
+    print("log(x) = " + str(int(logaritmo)))
     # FIN
     return
 
